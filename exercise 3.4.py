7be103490a0b9b20ec423c9d998b005e588a5d94
Nitesh Jha <nitesh7258@gmail.com>
2021-02-23 10:20:40 +0530

    day 3 updated

--- exercise 3.4.py
+++ exercise 3.4.py
@@ -8,42 +8,33 @@ extra_cheese = input("Do you want extra cheese? Y or N:  ")
 #Write your code below this line 👇
 
 if size == "S":
-  print("small pizza costs $15")
-
-  want_pepperoni = input("Do you want pepperoni? Y or N: ")
-  if want_pepperoni == "Y":
-    want_cheese = input("Do you want cheese? Y or N: ")
-    if want_cheese == "Y":
+  
+  if add_pepperoni == "Y":
+    if extra_cheese == "Y":
       print("your total bill amount is $18")
     else:
       print("your total bill amount is $17")
   else:
-    print("your total bill amount is $15")
+    print("\nyour total bill amount is $15")
 
 elif size == "M":
-  print("medium pizza is $20")
 
-  want_pepperoni = input("Do you want pepperoni? Y or N: ")
-  if want_pepperoni == "Y":
-    want_cheese = input("Do you want cheese? Y or N: ")
-    if want_cheese == "Y":
+  if add_pepperoni == "Y":
+    if extra_cheese == "Y":
       print("your total bill amount is $24")
     else:
       print("your total bill amount is $23")
   else:
-    print("your total bill amount is $20")
+    print("\nyour total bill amount is $20")
 
 else:
-  print("large pizza is $25")
 
-  want_pepperoni = input("Do you want pepperoni? Y or N: ")
-  if want_pepperoni == "Y":
-    want_cheese = input("Do you want cheese? Y or N: ")
-    if want_cheese == "Y":
+  if add_pepperoni == "Y":
+    if extra_cheese == "Y":
       print("your total bill amount is $29")
     else:
       print("your total bill amount is $28")
   else:
-    print("your total bill amount is $25") 
+    print("\nyour total bill amount is $25") 
 
 print("We are happy to serve you")
